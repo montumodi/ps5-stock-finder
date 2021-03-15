56416033901313d5d00bf39f8dffaf5978906844
Ashish Modi <ashish.modi@reedbusiness.com>
2021-03-15 14:39:01 +0000

    changed amazon url

--- providers/amazon.js
+++ providers/amazon.js
@@ -1,6 +1,6 @@
 const { getRawHtml } = require("../requestMaker");
 
-const url = "https://www.amazon.co.uk/gp/product/B08H95Y452?pf_rd_r=AAFY479RP90BA748F5ZA&pf_rd_p=6e878984-68d5-4fd2-b7b3-7bc79d9c8b60&pd_rd_r=fdb77a18-a826-4c59-8de4-7dc67719516a&pd_rd_w=Zimig&pd_rd_wg=faYGo&ref_=pd_gw_unk";
+const url = "https://www.amazon.co.uk/PlayStation-9395003-5-Console/dp/B08H95Y452/";
 
 async function process() {
     const rawHtml = await getRawHtml(url);
